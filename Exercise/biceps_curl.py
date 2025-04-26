525530800ed847bdb78b17ec8fdf9c75b72ec265
jitendeR-CODE1 <vilen7305@gmail.com>
2025-04-26 19:39:36 +0530

    Update biceps_curl.py

--- Exercise/biceps_curl.py
+++ Exercise/biceps_curl.py
@@ -1,28 +1,65 @@
+import streamlit as st
+import cv2
 import numpy as np
+from ultralytics import YOLO
+import math
 
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
 
-def calculate_angle(p1, p2, p3):
-    v1 = np.array(p1) - np.array(p2)
-    v2 = np.array(p3) - np.array(p2)
-    cosine_angle = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-    angle = np.degrees(np.arccos(np.clip(cosine_angle, -1.0, 1.0)))
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
     return angle
 
+def run():
+    st.title('Bicep Curl Counter')
 
-def detect(keypoints, state, reps, threshold=50):
-     if keypoints is None or len(keypoints) < 12:
-        return "No detection", reps, state
+    uploaded_file = st.file_uploader("Upload a video", type=["mp4", "mov", "avi", "mkv"])
+    if uploaded_file is not None:
+        tfile = open('temp.mp4', 'wb')
+        tfile.write(uploaded_file.read())
+        tfile.close()
 
-    r_shoulder = keypoints[5]
-    r_elbow = keypoints[7]
-    r_hip = keypoints[11]
+        cap = cv2.VideoCapture('temp.mp4')
+        model = YOLO('yolov8n-pose.pt')
 
-    angle = calculate_angle(r_shoulder, r_elbow, r_hip)
+        counter = 0
+        stage = None
+        stframe = st.empty()
 
-    if angle < 90:
-        if angle > 150 and state == "down":
-            state = "up"
-            reps += 1
-        elif angle < 90 and state == "up":
-            state = "down"
-    return "Bicep Curls", reps, state
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            results = model(frame, show=False)
+            keypoints = results[0].keypoints.xy.cpu().numpy()
+
+            if keypoints.size != 0:
+                shoulder = keypoints[0][5]
+                elbow = keypoints[0][7]
+                wrist = keypoints[0][9]
+
+                angle = calculate_angle(shoulder, elbow, wrist)
+
+                if angle > 160:
+                    stage = "down"
+                if angle < 45 and stage == 'down':
+                    stage = "up"
+                    counter += 1
+
+                cv2.putText(frame, str(counter),
+                            (50, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            stframe.image(frame, channels="RGB", use_column_width=True)
+
+        cap.release()
